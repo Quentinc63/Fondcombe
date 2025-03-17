5522517e1f402c4382fe487b16cf1279219ebf90
Léo Desktop <peyronnet.leo@gmail.com>
2025-03-17 09:29:34 +0100

    feat: titres requetes

--- requetes.sql
+++ requetes.sql
@@ -1,13 +1,15 @@
+-- Lister toutes les personnes séjournant à Fondcombe avec leurs informations de chambre.
 SELECT * FROM personnes 
 JOIN occupations ON personnes.id = occupations.utilisateur_id
 JOIN chambres ON occupations.chambre_id = chambres.id
 
+-- 	Trouver le nombre total de personnes pour chaque statut
 SELECT COUNT(*) AS nombres_par_race , races.nom AS nom_race
 FROM personnes 
 JOIN races  ON personnes.idRace = races.id
 GROUP BY races.nom 
 
-
+--	Rechercher toutes les quêtes assignées à une personne spécifique.
 SELECT *
 FROM personnes 
 JOIN aventuriers ON personnes.id = aventuriers.idPersonne
@@ -15,6 +17,8 @@ JOIN quetes ON aventuriers.idQuete = quetes.id
 WHERE personnes.id = 4
 -- placeholder pour la personne n4 (pcq il a plusieurs quetes)
 
+
+--	Rechercher toutes les personnes assignées à une quête spécifique.
 SELECT * 
 FROM quetes
 JOIN aventuriers ON quetes.id = aventuriers.idQuete
@@ -23,6 +27,7 @@ WHERE quetes.id = 1
 -- placeholder pour la quete n1
 
 
+--	Identifier la chambre disponible ayant la plus grande capacité à Fondcombe.
 SELECT chambres.id, chambres.numero, chambres.etage, chambres.capacite
 FROM chambres
 LEFT JOIN occupations ON chambres.id = occupations.idChambre AND (occupations.dateFin IS NULL OR occupations.dateFin >= CURDATE())
